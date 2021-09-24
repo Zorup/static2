c219157511aced65bf24e7cc1b090ffb8b1b9f9c
김한성 <khs960616>
2021-09-24 19:08:10 +0900

    좋아요 기능 연동

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,17 +1,34 @@
 import CreateComment from "./CreateComment";
 import Comment from "./Comment";
-
+import axios from 'axios';
+import {useState, useEffect} from "react"
 
 function Post({post}){
     const commentList = post.comments.map(item => (
         <Comment comment={item} key={item.commentId}/>
     ));
 
+    const [likes, setLikes] = useState(post.likes);
+
+    const clickLikeHandler = async()=>{
+        try{
+            const response = await axios.post(
+                `http://localhost:8081/main/v1/like?postId=${post.postId}`,
+                null,
+                {
+                    withCredentials: true
+                }
+            );
+            setLikes(response.data.data)
+        }catch(e){
+        }
+    }
+
     return(
         <>
             <div className="bg-white p-2">
                 <div className="d-flex flex-row user-info">
-                    <img className="rounded-circle" src="img/undraw_profile.svg" width="60" height="60"></img>
+                    <img className="rounded-circle" src="img/undraw_profile.svg" width="60" height="60"/>
                     <div className="d-flex flex-column justify-content-start ml-2">
                         <span className="d-block font-weight-bold name"> {post.userName} </span>
                         <span className="date text-black-50">{post.createdDate} </span>
@@ -19,6 +36,21 @@ function Post({post}){
                 </div>
                 <div className="mt-2">
                     <p className="comment-text">{post.content}</p>
+                </div>
+
+                <div className="bg-white">
+                    <div className="d-flex flex-row fs-12">
+                        <a className="p-2 like-button">
+                            <i className="fa fa-thumbs-up"></i>
+                            <span className="testLike">{likes}</span>
+                            <span className="ml-1" onClick={clickLikeHandler}>Like</span>
+                        </a>
+                        <a className="p-2 ">
+                            <i className="fa fa-comment"></i>
+                            <span>{post.comments.length}</span>
+                            <span className="ml-1">Comment</span>
+                        </a>
+                    </div>
                 </div>
             </div>
             {commentList}
