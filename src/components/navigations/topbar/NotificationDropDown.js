ee3979cc2bc07c5ddccdeb93a94e471c1a9a89a5
김한성 <khs960616>
2021-10-03 03:29:05 +0900

    TODO :: 클릭시 읽기 처리

--- src/components/navigations/topbar/NotificationDropDown.js
+++ src/components/navigations/topbar/NotificationDropDown.js
@@ -1,11 +1,22 @@
+import React, { useCallback} from 'react';
+import './Notification.css'
 function NotificationDropDown({item}){
+    console.log(item.readYn);
+    const onClickHandler = useCallback((e)=>{
+        // drop 다운 내부 클릭시 드롭다운 리스트가 자동으로 풀림 방지 
+        e.stopPropagation();
+
+        
+        alert("click!");
+    });
     return(
-        <a className="dropdown-item d-flex align-items-center" href="#">
+        <a className="dropdown-item d-flex align-items-center" href="#" onClick={onClickHandler}>
             <div>
                 <div className="small text-gray-500">
                     {item.createDate.month} {item.createDate.dayOfMonth}, {item.createDate.year}
                 </div>
-                <span className="font-weight-bold">{item.content}</span>
+                <span className="font-weight-bold">{item.content}  </span>
+                { item.readYn ? null : <span className="badge badge-danger badge-counter"> new </span> }
             </div>
         </a>
     )
